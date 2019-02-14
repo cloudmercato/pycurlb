033fb5beb20223379e2dd49274f2c0a2eb35dd55
Anthony Monthe <anthony.monthe@gmail.com>
2019-02-14 03:36:24 +0000

    Added options

--- pycurlb.py
+++ pycurlb.py
@@ -9,34 +9,36 @@ INFO_KEYS = [
     'content_type',
     'ftp_entry_path',
     'http_code',
-    'http_connect',
+    'http_connectcode',
     'http_version',
     'local_ip',
     'local_port',
     'num_connects',
-    'num_redirects',
-    'proxy_ssl_verify_result',
+    'redirect_count',
+    'redirect_time',
+    'ssl_verifyresult',
     'redirect_url',
-    'remote_ip',
-    'remote_port',
-    'scheme',
+    'primary_ip',
+    'primary_port',
     'size_download',
-    'size_header',
-    'size_request',
     'size_upload',
+    'header_size',
+    'request_size',
     'speed_download',
     'speed_upload',
-    'ssl_verify_result',
-    'time_appconnect',
-    'time_connect',
-    'time_namelookup',
-    'time_pretransfer',
-    'time_redirect',
-    'time_starttransfer',
-    'time_total',
-    'url_effective',
+    'content_length_download',
+    'content_length_upload',
+    'appconnect_time',
+    'connect_time',
+    'namelookup_time',
+    'pretransfer_time',
+    'starttransfer_time',
+    'total_time',
+    'effective_url',
     'stderr',
     'stdout',
+    'os_errno',
+    'ssl_engines',
 ]
 
 
@@ -49,28 +51,33 @@ class Curler:
         info = {}
         for key in INFO_KEYS:
             key_upper = key.upper()
-            if hasattr(self.curl, key_upper):
-                attr = getattr(self.curl, key_upper)
-                try:
-                    value = self.curl.getinfo(attr)
-                    info[key] = value
-                except ValueError:
-                    pass
+            if not hasattr(self.curl, key_upper):
+                continue
+            attr = getattr(self.curl, key_upper)
+            try:
+                value = self.curl.getinfo(attr)
+                info[key] = value
+            except ValueError:
+                pass
         return info
 
-    def perform(self, url, verbose=False, method=None):
+    def perform(self, url, verbose=False, insecure=True, method=None):
         self.curl.setopt(self.curl.URL, url)
         self.curl.setopt(self.curl.WRITEDATA, self.buffer)
         self.curl.setopt(self.curl.VERBOSE, verbose)
+        self.curl.setopt(self.curl.SSL_VERIFYPEER, insecure)
         self.curl.perform()
-        # self.curl.close()
         return self._extract_info()
+
+    def close(self):
+        self.curl.close()
 
 
 def main():
     # Argparse
     parser = argparse.ArgumentParser(description="Get statistics from curl request")
     parser.add_argument('url')
+    parser.add_argument('-k', '--insecure', action='store_false')
     parser.add_argument('-X', '--request')
     parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
@@ -80,7 +87,9 @@ def main():
         method=args.request,
         url=args.url,
         verbose=args.verbose,
+        insecure=args.insecure,
     )
+    curler.close()
     # Print
     sys.stdout.write(json.dumps(info, indent=2, sort_keys=True))
 
