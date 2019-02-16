86dba970d3aa977be21aea444552df383bdecb48
Anthony Monthe <anthony.monthe@gmail.com>
2019-02-16 18:52:11 +0000

    Added HTTPPOST data & output file

--- pycurlb.py
+++ pycurlb.py
@@ -6,6 +6,7 @@ import sys
 import argparse
 import json
 from io import BytesIO
+from shutil import copyfileobj
 
 import pycurl
 
@@ -66,7 +67,8 @@ INFO_KEYS = [
 class Curler:
     def __init__(self):
         self.curl = pycurl.Curl()
-        self.buffer = BytesIO()
+        self.response_buffer = BytesIO()
+        self.request_buffer = BytesIO()
 
     def _extract_info(self):
         info = {}
@@ -82,16 +84,23 @@ class Curler:
                 pass
         return info
 
-    def perform(self, url, verbose=False, insecure=True, method=None, compressed=False):
+    def perform(self, url, verbose=False, insecure=True, method=None, compressed=False,
+                connect_timeout=300, connect_timeout_ms=None, data=None):
         self.curl.setopt(self.curl.URL, url)
         self.curl.setopt(self.curl.CUSTOMREQUEST, method)
-        self.curl.setopt(self.curl.WRITEDATA, self.buffer)
+        self.curl.setopt(self.curl.WRITEDATA, self.response_buffer)
         self.curl.setopt(self.curl.VERBOSE, verbose)
         self.curl.setopt(self.curl.SSL_VERIFYPEER, insecure)
+        self.curl.setopt(self.curl.CONNECTTIMEOUT, connect_timeout)
+        if connect_timeout_ms is not None:
+            self.curl.setopt(self.curl.CONNECTTIMEOUT_MS, connect_timeout_ms)
         if compressed:
             self.curl.setopt(self.curl.ACCEPT_ENCODING, "gzip,deflate")
-        # self.curl.setopt(self.curl.UPLOAD, True)
-        # self.curl.setopt(self.curl.READFUNCTION, BytesIO(b'\n').read)
+        if data is not None:
+            self.request_buffer.write(data.encode())
+            self.request_buffer.seek(0)
+            self.curl.setopt(self.curl.UPLOAD, True)
+            self.curl.setopt(self.curl.READFUNCTION, self.request_buffer.read)
         # self.curl.setopt(self.curl.INFILESIZE, 0)
         self.curl.perform()
         return self._extract_info()
@@ -110,16 +119,24 @@ def main():
     parser = argparse.ArgumentParser(description="Get statistics from curl request")
     parser.add_argument('url')
     parser.add_argument('--compressed', action='store_true',
-                        help="Request compressed response")
+                        help="Request compressed response.")
+    parser.add_argument('--connect-timeout', default=300, type=int,
+                        help="Maximum time allowed for connection in seconds.")
+    parser.add_argument('--connect-timeout-ms', default=None, type=int,
+                        help="Maximum time allowed for connection in milliseconds.")
+    parser.add_argument('-d', '--data', default=None,
+                        help="HTTP POST data.")
     parser.add_argument('-k', '--insecure', action='store_false',
-                        help="Allow insecure server connections when using SSL")
+                        help="Allow insecure server connections when using SSL.")
     parser.add_argument('-X', '--request',
-                        help="Specify request command to use")
+                        help="Specify request command to use.")
     parser.add_argument('-v', '--verbose', action='store_true',
-                        help="Make the operation more talkative")
+                        help="Make the operation more talkative.")
     parser.add_argument('-V', '--version', action='store_const', const=get_version,
                         default=None,
-                        help="Show version number and quit")
+                        help="Show version number and quit.")
+    parser.add_argument('-w', '--write-out', default=None,
+                        help="Write request output to specified file.")
     args = parser.parse_args()
     # Meta
     if args.version is not None:
@@ -129,15 +146,22 @@ def main():
     # Perform
     curler = Curler()
     info = curler.perform(
+        compressed=args.compressed,
+        connect_timeout=args.connect_timeout,
+        connect_timeout_ms=args.connect_timeout_ms,
+        data=args.data,
+        insecure=args.insecure,
         method=args.request,
         url=args.url,
         verbose=args.verbose,
-        insecure=args.insecure,
-        compressed=args.compressed,
     )
     curler.close()
     # Print
     sys.stdout.write(json.dumps(info, indent=2, sort_keys=True))
+    if args.write_out is not None:
+        curler.response_buffer.seek(0)
+        with open(args.write_out, 'wb') as dst_file:
+            copyfileobj(curler.response_buffer, dst_file)
 
 if __name__ == '__main__':
     main()
