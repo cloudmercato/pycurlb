8cd881bbd9bdf4cf0a5488b6db98ffa478ffbe82
Anthony Monthe <anthony.monthe@gmail.com>
2019-02-16 23:48:17 +0000

    Added console options

--- pycurlb.py
+++ pycurlb.py
@@ -10,7 +10,7 @@ from shutil import copyfileobj
 
 import pycurl
 
-VERSION = (0, 1, 0)
+VERSION = (0, 2, 0)
 __version__ = '.'.join([str(i) for i in VERSION])
 __author__ = "Anthony Monthe (ZuluPro)"
 __email__ = 'amonthe@cloudspectator.com'
@@ -85,15 +85,30 @@ class Curler:
         return info
 
     def perform(self, url, verbose=False, insecure=True, method=None, compressed=False,
-                connect_timeout=300, connect_timeout_ms=None, data=None):
+                connect_timeout=300, connect_timeout_ms=None, data=None, headers=None,
+                user=None, user_agent=None, max_time=0, max_time_ms=None, cookie=None,
+                cookie_jar=None):
         self.curl.setopt(self.curl.URL, url)
         self.curl.setopt(self.curl.CUSTOMREQUEST, method)
         self.curl.setopt(self.curl.WRITEDATA, self.response_buffer)
         self.curl.setopt(self.curl.VERBOSE, verbose)
         self.curl.setopt(self.curl.SSL_VERIFYPEER, insecure)
         self.curl.setopt(self.curl.CONNECTTIMEOUT, connect_timeout)
+        self.curl.setopt(self.curl.TIMEOUT, max_time)
+        if cookie:
+            self.curl.setopt(self.curl.COOKIE, cookie)
+        if cookie_jar:
+            self.curl.setopt(self.curl.COOKIEJAR, cookie_jar)
+        if headers:
+            self.curl.setopt(self.curl.HTTPHEADER, headers)
+        if user_agent is not None:
+            self.curl.setopt(self.curl.USERAGENT, user_agent)
+        if user is not None:
+            self.curl.setopt(self.curl.USERPWD, user_agent)
         if connect_timeout_ms is not None:
             self.curl.setopt(self.curl.CONNECTTIMEOUT_MS, connect_timeout_ms)
+        if max_time_ms is not None:
+            self.curl.setopt(self.curl.TIMEOUT_MS, max_time_ms)
         if compressed:
             self.curl.setopt(self.curl.ACCEPT_ENCODING, "gzip,deflate")
         if data is not None:
@@ -116,51 +131,86 @@ def get_version():
 
 def main():
     # Argparse
-    parser = argparse.ArgumentParser(description="Get statistics from curl request")
+    parser = argparse.ArgumentParser(
+        prog='curlb',
+        description="Get statistics from curl request"
+    )
     parser.add_argument('url')
     parser.add_argument('--compressed', action='store_true',
                         help="Request compressed response.")
-    parser.add_argument('--connect-timeout', default=300, type=int,
+    parser.add_argument('--connect-timeout', default=300, type=int, metavar='<seconds>',
                         help="Maximum time allowed for connection in seconds.")
-    parser.add_argument('--connect-timeout-ms', default=None, type=int,
+    parser.add_argument('--connect-timeout-ms', default=None, type=int, metavar='<ms>',
                         help="Maximum time allowed for connection in milliseconds.")
-    parser.add_argument('-d', '--data', default=None,
+    parser.add_argument('-b', '--cookie', default=None, metavar='<data>',
+                        help="Send cookies from string/file")
+    parser.add_argument('-c', '--cookie-jar', default=None, metavar='<filename>',
+                        help="Write cookies to <filename> after operation")
+    parser.add_argument('-d', '--data', default=None, metavar='<data>',
                         help="HTTP POST data.")
+    parser.add_argument('-H', '--header', default=None, nargs='*', metavar='<header/@file>',
+                        help="Pass custom header(s) to server.")
     parser.add_argument('-k', '--insecure', action='store_false',
                         help="Allow insecure server connections when using SSL.")
-    parser.add_argument('-X', '--request',
+    parser.add_argument('-m', '--max-time', default=0, type=int, metavar='<seconds>',
+                        help="Maximum time allowed for the transfer in seconds.")
+    parser.add_argument('--max-time-ms', default=None, type=int, metavar='<ms>',
+                        help="Maximum time allowed for the transfer in milliseconds.")
+    parser.add_argument('-o', '--output', default=None, metavar='<file>',
+                        help="Write response output to specified file.")
+    parser.add_argument('-X', '--request', metavar='<command>',
                         help="Specify request command to use.")
+    parser.add_argument('-A', '--user-agent', default=None, metavar='<name>',
+                        help="Send User-Agent to server.")
+    parser.add_argument('-u', '--user', default=None, metavar='<user:password>',
+                        help="<user:password> Server user and password")
     parser.add_argument('-v', '--verbose', action='store_true',
                         help="Make the operation more talkative.")
     parser.add_argument('-V', '--version', action='store_const', const=get_version,
                         default=None,
                         help="Show version number and quit.")
-    parser.add_argument('-w', '--write-out', default=None,
-                        help="Write request output to specified file.")
     args = parser.parse_args()
     # Meta
     if args.version is not None:
         sys.stdout.write(args.version())
         sys.stdout.write('\n')
         sys.exit(0)
+    # Set headers
+    headers = []
+    for header in args.header:
+        if header.startswith('@'):
+            try:
+                with open(header, 'r') as header_fd:
+                    headers.extend(header_fd.readlines())
+            except Exception as err:  # noqa
+                sys.stderr.write("Warning: Failed to open %s: %s" % (header, err))
+        else:
+            headers.append(header)
     # Perform
     curler = Curler()
     info = curler.perform(
         compressed=args.compressed,
         connect_timeout=args.connect_timeout,
         connect_timeout_ms=args.connect_timeout_ms,
+        cookie=args.cookie,
+        cookie_jar=args.cookie_jar,
+        max_time=args.max_time,
+        max_time_ms=args.max_time_ms,
         data=args.data,
+        headers=headers,
         insecure=args.insecure,
         method=args.request,
         url=args.url,
+        user=args.user,
+        user_agent=args.user_agent,
         verbose=args.verbose,
     )
     curler.close()
     # Print
     sys.stdout.write(json.dumps(info, indent=2, sort_keys=True))
-    if args.write_out is not None:
+    if args.output is not None:
         curler.response_buffer.seek(0)
-        with open(args.write_out, 'wb') as dst_file:
+        with open(args.output, 'wb') as dst_file:
             copyfileobj(curler.response_buffer, dst_file)
 
 if __name__ == '__main__':
